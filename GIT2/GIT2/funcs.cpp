c6b7149cfd2942a94ff18fc092253224211b84a1
Leontovich Nikita <varygstavr@yandex.ru>
2019-12-05 19:26:13 +0300

    minus and umnogenie

--- GIT2/GIT2/funcs.cpp
+++ GIT2/GIT2/funcs.cpp
@@ -1,1 +1,6 @@
+//funcs.cpp 
 #include "pch.h"
+#include "funcs.h" 
+#include <cmath>  
+double Plus(double a, double b) { return a + b; }
+double Del(double a, double b) { return a / b; }
--- GIT2/GIT2/funcs.h
+++ GIT2/GIT2/funcs.h
@@ -1,1 +1,5 @@
-#pragma once
+#ifndef __FUNCS_H__
+#define __FUNCS_H__   
+double Plus(double, double);
+double Del(double, double);
+#endif
